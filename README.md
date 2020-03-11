8cac51820ae85ef61e280887adcc3e3eec045d31
David Chen <chen7david@me.com>
2020-03-11 14:25:49 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# federation-response
-coming soon
+# Federation-Response (FR)
+The Federation Response module is a predefined response structure that is used by APIs in the API-Federation Ecosystem (AFE) to serve responses that conform to the R1-Response structre as defined by AFE.
+
+### 1. Getting Started
+You only need to do two things if you want to use federation-response in your project:
+- require in your <code>store.js</code> as storefile
+- npm install federation-response in your project
+- require Response from federation-response
+- pass in the storefile as an argumnet to Response and new to create a new Response instance
+The code below illustrates the steps above:
+```js
+const storefile = require('./store.json')
+const { Response } = require('federation-response')
+
+const fres = new Response(storefile)
+    .payloadTo({user:{id:1}, roles:[1,2,3]})
+    .message('invalid_password')
+    .done()
+
+console.log(fres)
+```
+The code above would yield teh response below:
+```js
+Response {
+  id: 7395,
+  isFederationResponse: true,
+  status: null,
+  lang: 'en',
+  payload: { user: { id: 1 }, roles: [ 1, 2, 3 ] },
+  details: [
+    {
+      code: 'invalid_password',
+      status: 422,
+      state: 'validation',
+      key: 'password',
+      message: 'the password you entered is invalid'
+    }
+  ],
+  directives: [],
+  created: '3/11/2020, 2:12:26 PM'
+}
+```
---
+++ index.js
@@ -0,0 +1,97 @@
+const dd = (val) => console.log(val)
+const { template } = require('lodash')
+const store = require('./store.json')
+class Response {
+    
+    constructor(storefile = null){
+        if(!store) throw('storefile required!')
+        this.id = Math.floor((Math.random() * 9000) + 1000)
+        this.isFederationResponse = true
+        this.status = null
+        this.lang = 'en'
+        this.payload = null
+        this.details = []
+        this.directives = []
+        this.created = new Date().toLocaleString()
+        this.store = storefile
+    }
+
+    statusTo(status){
+        this.status = status
+        return this
+    }
+
+    langTo(lang){
+        this.lang = lang
+        return this
+    }
+
+    isSilent(){
+        this.silent = true
+    }
+
+    message(code, data = null){
+        let message = code
+        if(this.store){
+            let notification = this.store.notifications.find(el => el.name == code)
+            if(!notification) notification = this.store.notifications
+                .find(el => el.name == 'invalid_message_code')
+            if(notification) message = this.mutate(notification, data)
+            if(message.state == 'error') message.message = `${message.message} - ${this.id}`
+        }
+        this.details.push(message)
+        return this
+    }
+
+    messageTo(message){
+        this.details.push(message)
+        return this
+    }
+
+    mutate(notification, data){
+        let detail = notification.details.find(el => el.lang == this.lang)
+        return {
+            code: notification.name,
+            status: notification.status,
+            state: notification.state,
+            key: detail.key,
+            message: data ? template(detail.message)(data) : detail.message
+        }
+    }
+
+    payloadTo(data){
+        this.payload = data
+        return this
+    }
+
+    directiveTo(directive){
+        this.directives.push(directive)
+        return this
+    }
+
+    done(){
+        delete this.store
+        return this
+    }
+
+    throw(){
+        throw(this)
+    }
+}
+
+// let fres = new Response(store)
+//     .payloadTo({user:{id:1}, roles:[1,2,3]})
+//     .message('invalid_password')
+//     .message('authenticated_user')
+//     .message('authenticated_user', {username: 'some-cool-name'})
+//     .message({first:'custom message'})
+//     .messageTo({message:'this is a custom message'})
+//     .done()
+
+    const fres = new Response(store)
+    .payloadTo({user:{id:1}, roles:[1,2,3]})
+    .message('invalid_password')
+    .done()
+
+dd(fres)
+module.exports = { Response }
